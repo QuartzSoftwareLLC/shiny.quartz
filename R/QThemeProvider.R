12dfaf732485c7b94948fb96a46c096b8ec595d6
Jacob Clarke <jacobclarke718@gmail.com>
2022-04-06 12:04:33 -0500

    added iddr theme colors

--- R/QThemeProvider.R
+++ R/QThemeProvider.R
@@ -1,10 +1,17 @@
-#' @export
-#' @importFrom shiny.mui ThemeProvider
+# " @export
+# " @importFrom shiny.mui ThemeProvider
 QThemeProvider <- function(...) {
     input <- list(defaultProps = list(fullWidth = T, size = "small"))
     ThemeProvider(
         ...,
-        theme = list(components = list(
+        theme = list(
+        palette = list(
+            primary = list(main = "#475C7A"),
+            secondary = list(main = "#D8737F"),
+            # background = list(paper = "#EFEFFF"),
+            warning = list(main = "#685D78")
+        ),
+        components = list(
             MuiTextField = input,
             MuiFormControl = input,
             MuiSelect = input
