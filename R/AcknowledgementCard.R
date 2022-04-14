d0dc2e66b40afd82ab98c8d0d1dea0b4f764c6ba
Jacob Clarke <jacobclarke718@gmail.com>
2022-04-14 16:46:50 -0500

    updated card to show avatars

--- R/AcknowledgementCard.R
+++ R/AcknowledgementCard.R
@@ -2,9 +2,15 @@
 #' @param authors A vector of author names.
 #' @export
 #' @example inst/examples/AcknowledgementCard.R
-AcknowledgementCard <- function(authors = c("Timothy L. Wiemken", "Jacob A. Clarke", "Christopher Prener", "Verna Welch", "John McLaughlin")) {
+#' @importFrom shiny.mui ListItemButton Link ListItemAvatar Avatar ListItemText
+AcknowledgementCard <- function(authors = list(
+    list(name = "Timothy L. Wiemken", src = "https://media-exp1.licdn.com/dms/image/C4E03AQE4JkLxHi1g7A/profile-displayphoto-shrink_400_400/0/1603549767246?e=1655337600&v=beta&t=mGGA7RxgQvdi0yIvbwiOJXiJ0uvM_YAm0-UuybyOp8g"),
+    list(name = "Jacob A. Clarke", src = "https://www.jacobaclarke.com/headshot_new_cropped.jpg", href = "https://www.jacobaclarke.com/"),
+    list(name = "Christopher Prener"),
+    list(name = "Verna Welch"),
+    list(name = "John McLaughlin"))) {
     QCard(
         title = "Acknowledgements",
-        shiny.mui::List(authors %>% lapply(\(x) QListItem(x))),
+        shiny.mui::List(authors %>% lapply(\(x) Link(href = x$href, ListItemButton(ListItemAvatar(Avatar(src = x$src)), ListItemText(primary = x$name)))))
     )
 }
