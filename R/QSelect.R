86848719aed039277bd828d7d2d65961008f6aac
Jacob Clarke <jacobclarke718@gmail.com>
2022-05-02 20:26:40 -0500

    changed qselect to native

--- R/QSelect.R
+++ R/QSelect.R
@@ -4,13 +4,14 @@
 #' @export
 #' @param options A list of key value pairs
 #' @importFrom shiny.mui Select.shinyInput
+#' @importFrom htmltools tags
 #' @import magrittr
 #' @example inst/examples/QSelect.R
 QSelect.shinyInput <- function(..., options, label = "") {
     children <- names(options) %>%
-        lapply(\(x) shiny.mui::MenuItem(value = x, options[[x]]))
+        lapply(\(x) tags$option(value = x, options[[x]]))
     shiny.mui::FormControl(
         shiny.mui::InputLabel(label),
-        Select.shinyInput(..., children, label = label)
+        Select.shinyInput(..., native = T, children, label = label)
     )
 }
