2e328ba075320dabccdc3b868ad09dfe5d4356f2
Jacob Clarke <jacobclarke@me.com>
2022-12-12 02:22:51 +0000

    added setup cache

--- R/Page.R
+++ R/Page.R
@@ -1,26 +1,35 @@
 #' Page
-#' 
+#'
 #' Generates a page with a gray background and a title at the top.
 #' Also applies qthemeprovider to the page.
-#' 
+#'
 #' @param title character the title of the page.
 #' @param ... a list of components to include in the page.
-#' 
+#'
 #' @examples
 #' component_example(Page("Title", "Hello there"))
-#' 
+#'
 #' @family layout
 #' @seealso [QThemeProvider()]
 #' @importFrom shiny tags HTML tagList
 #' @export
 Page <- function(title, ...) {
     tagList(
-        tags$head(tags$style(
-            HTML(paste0("
-           body {
-          background-color: ",
-          shiny.quartz::background,
-          ";}")))),
+        tags$head(
+            tags$style(
+                sprintf("
+        body {
+            background-color: %s;
+        }
+        .waiter-overlay-content{
+            position: absolute;
+            top: 30px; /*30 pixels from the top*/
+            left: 48%; /*48% from the left*/
+        }
+          ", shiny.quartz::background)
+            ),
+            waiter::autoWaiter()
+        ),
         tags$body(
             QThemeProvider(
                 shiny.mui::Typography(variant = "h4", title, color = "primary", sx = list(m = 1)),
---
+++ R/Redis.R
@@ -0,0 +1,54 @@
+RedisCache <- R6::R6Class("RedisCache",
+    public = list(
+        initialize = function(..., namespace = NULL) {
+            private$r <- redux::hiredis(...,
+                host = Sys.getenv("REDIS_HOST")
+            )
+
+            private$r$AUTH(Sys.getenv("REDIS_AUTH"))
+            # Configure redis as a cache with a 20 MB capacity
+            private$r$CONFIG_SET("maxmemory", "20mb")
+            private$r$CONFIG_SET("maxmemory-policy", "allkeys-lru")
+            private$namespace <- namespace
+            private$cache <- list()
+        },
+        get = function(key) {
+            print("using cache")
+            key <- paste0(private$namespace, "-", key)
+            local_value <- private$cache[[key]]
+            if (!is.null(local_value)) {
+                print("using local")
+                return(local_value)
+            }
+            s_value <- private$r$GET(key)
+            if (is.null(s_value)) {
+                return(structure(list(), class = "key_missing"))
+            }
+            val <- unserialize(s_value)
+            private$cache[[key]] <- val
+
+            val
+        },
+        set = function(key, value) {
+            print("setting")
+            key <- paste0(private$namespace, "-", key)
+            s_value <- serialize(value, NULL)
+            private$r$SET(key, s_value)
+        }
+    ),
+    private = list(
+        r = NULL,
+        namespace = NULL,
+        cache = NULL
+    )
+)
+
+#' Setup Cache
+#' @export
+#' 
+#' Please set REDIS_HOST and REDIS_AUTH in your settings
+setup_cache <- function(namespace) {
+    if (system.file(package = "redux") %>% `!=`("")) {
+        shinyOptions(cache = RedisCache$new(namespace = namespace))
+    }
+}
